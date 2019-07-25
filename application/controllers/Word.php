96c13157c96b02dea5a8d1eed2eba5a234ebd12a
khoronghejo <you@example.com>
2019-07-25 15:36:43 +0700

    word

--- application/controllers/Word.php
+++ application/controllers/Word.php
@@ -6,32 +6,33 @@ class Word extends CI_Controller {
 	public function index()
 	{
 
-$phpWord = new \PhpOffice\PhpWord\PhpWord();
-// New portrait section
-$section = $phpWord->addSection();
-// Add first page header
-$header = $section->addHeader();
-$header->firstPage();
-$table = $header->addTable();
-$table->addRow();
-$cell = $table->addCell(4500);
-$textrun = $cell->addTextRun();
-$textrun->addText('This is the header with ');
-// Write some text
-$section->addTextBreak();
-$section->addText('Sunt sunt irure ad adipisicing mollit. Fugiat dolore ut in cillum proident sunt commodo velit voluptate dolore consequat. Id officia minim amet sunt deserunt ad deserunt dolor ullamco irure amet eiusmod elit. Magna nulla duis culpa ipsum in.
+		$phpWord = new PhpWord();
+		$title = 'rStyle';
+		$phpWord->addFontStyle($title, array('bold' => true, 'italic' => false, 'size' => 11, 'font'=>'arial'));
+		$paragraphStyleName = 'pStyle';
+		$phpWord->addParagraphStyle($paragraphStyleName, array('alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER, 'spaceAfter' => 100));
+		$phpWord->addTitleStyle(1, array('bold' => true), array('spaceAfter' => 240));
+		// New portrait section
+		$section = $phpWord->addSection();
+		// Simple text
+		$section->addText('KEMENTERIAN HUKUM DAN HAK ASASI MANUSIA', $title, $paragraphStyleName);
+		$section->addText('INSPEKTORAT JENDERAL', $title, $paragraphStyleName);
+		$section->addTextBreak();
+		$section->addText('NOTA DINAS', $fontStyleName, $paragraphStyleName);
 
-Incididunt qui consequat aliqua Lorem veniam incididunt cillum ea nisi eu. Labore anim exercitation laboris sit sit exercitation ex enim cupidatat nostrud ad occaecat. In voluptate id do id aute et dolor dolore cupidatat ad ut laboris. Culpa deserunt ut Lorem cupidatat amet.');
-// Save file
-$writer = new Word2007($phpWord);
-		
-		$filename = 'simple';
-		
-		header('Content-Type: application/msword');
-        	header('Content-Disposition: attachment;filename="'. $filename .'.docx"'); 
-		header('Cache-Control: max-age=0');
-		
-		$writer->save('php://output');
+		// Two text break
+		$section->addTextBreak(2);
+		// Define styles
+		$section->addTextBreak();
+				$writer = new Word2007($phpWord);
+				
+				$filename = 'simple';
+				
+				header('Content-Type: application/msword');
+					header('Content-Disposition: attachment;filename="'. $filename .'.docx"'); 
+				header('Cache-Control: max-age=0');
+				
+				$writer->save('php://output');
 
 
 	}
