7c380cc657977de1e215ff85ee97563a51dbf2fd
khoronghejo <you@example.com>
2019-07-25 16:25:34 +0700

    komit baru

--- application/controllers/Word.php
+++ application/controllers/Word.php
@@ -45,7 +45,7 @@ class Word extends CI_Controller {
 		$section->addText('INSPEKTORAT JENDERAL', $title, $paragraphStyleName);
 		$section->addTextBreak(null,$nodinStyle, $paragraphStyleName);
 		$section->addText('NOTA DINAS', $nodinStyle, $paragraphStyleName);
-		$section->addText('NOMOR : ITJ.1.UM.01.01-XXXX/VII/2019 ', $nodinStyle, $paragraphStyleName);
+		$section->addText('Nomor : ITJ.1.UM.01.01-XXXX/VII/2019 ', $nodinStyle, $paragraphStyleName);
 		$section->addTextBreak(1);
 		$table = $section->addTable();
 		$table->addRow();
@@ -68,6 +68,12 @@ class Word extends CI_Controller {
 		$table->addCell(2500)->addText("Tanggal",$tableHeader, $paragraphTable);
 		$table->addCell(200)->addText(":",$tableHeader, $paragraphTable);
 		$table->addCell(7300)->addText("",$tableHeader, $paragraphTable);
+		$section->addTextBreak(1);
+		$section->addText('Aliquip amet cupidatat reprehenderit proident. Non quis mollit amet ea ut ea eiusmod. Labore laborum dolor nisi deserunt anim reprehenderit esse ipsum cupidatat. Velit minim dolore adipisicing eu velit adipisicing. Commodo dolore amet qui et ipsum mollit irure.
+
+		Ea consequat ea deserunt quis occaecat sunt est ea pariatur ullamco mollit occaecat id proident. Duis nostrud laboris proident id laborum fugiat aliquip sit Lorem voluptate. Exercitation esse incididunt occaecat pariatur. Sint fugiat eiusmod do reprehenderit non qui dolore nulla eiusmod. Veniam labore nulla eiusmod ex consectetur occaecat aliquip ea id quis aliqua non id excepteur.
+
+		Consequat eu duis sint qui dolor voluptate cillum ut laborum esse exercitation consectetur. Nostrud sint cupidatat consequat ipsum voluptate cillum. Proident labore elit culpa labore nostrud irure ipsum deserunt qui. Veniam Lorem ea non occaecat exercitation incididunt aliquip culpa ullamco culpa ullamco pariatur consequat velit. Ipsum sunt ex tempor magna in consequat minim nisi in. Laboris excepteur dolor magna tempor consequat.');
 		
 				$writer = new Word2007($phpWord);
 				
